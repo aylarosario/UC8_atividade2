6264545107719d4b0ef669eaaec1ded437f23cc3
aylarosario <98574408+aylarosario@users.noreply.github.com>
2022-02-26 16:50:06 -0300

    Add files via upload

---
+++ atividade_online2.js
@@ -0,0 +1,47 @@
+/*
+* Se a data do evento for posterior à data atual, permitir o evento;
+* senão, alertar que o cadastro não será permitido por data inválida.
+*/
+
+const evento01 = "nome do evento";
+let dataDoEvento = "26/02/2022";
+if (dataDoEvento == 26/02/2022) {
+    console.log("Selecionar evento")
+} else {
+    console.log("Data inválida")
+}
+let selecioneHorario = "09:00";
+if (selecioneHorario<= "09:00") {
+    console.log("Horário indisponível")
+} else {
+    console.log("Horário selecionado")
+}
+let nomeDePalestrantes = "Adicionar nome de Palestrante";
+let idadeDoPalestrante = "18";
+if (idadeDoPalestrante == "<18"){
+    console.log("Evento apenas para maiores de 18 anos")
+}
+
+/*Se o participante for maior de 18 anos, permitir o cadastro;
+*senão, alertar que o cadastro não é permitido pela idade.
+* Listar participantes e palestrantes por evento
+*/
+
+let nomeDeParticipante = "Ayla";
+let idadeDoParticipante = "18";
+if (idadeDoParticipante == "<18"){
+    console.log("Evento apenas para maiores de 18 anos")
+} else {
+    console.log("Selecione o evento")
+}
+let quantidadeDeParticipantes = "100";
+if (quantidadeDeParticipantes<=100) {
+    console.log("Participação confirmada")
+} else {
+    console.log("Limite de participantes excedido!")
+}
+
+//final
+console.log("lista de palestrantes")
+console.log("lista de participantes")
+console.log("data do evento")
